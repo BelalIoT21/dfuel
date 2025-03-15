53b21d089f750d34e9ac5746458ad89b57d3a997
Belal <b.l.mishmish@gmail.com>
2025-03-15 11:30:21 +0400

    f

--- src/hooks/usePasswordResetFunctions.ts
+++
@@ -1,8 +0,0 @@
-
-// This hook has been deprecated as password reset functionality has been removed
-export const usePasswordResetFunctions = () => {
-  return {
-    requestPasswordReset: async () => false,
-    resetPassword: async () => false
-  };
-};
