0f0806638436e52669252cb205464e2fd96e81d3
Belal <b.l.mishmish@gmail.com>
2025-04-01 21:27:13 +0100

    app fixed and updated

--- server/src/server.ts
+++ server/src/server.ts
@@ -97,7 +97,7 @@ createAdminUser()
     console.error('Error in startup process:', error);
   });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
